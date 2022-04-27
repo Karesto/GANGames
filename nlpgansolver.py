c28351d566c16c032d75cf60afd716e60302e78e
Karesto <ahmedhachoukarah@gmail.com>
2022-04-27 10:51:17 +0200

    first commit

---
+++ nlpgansolver.py
@@ -0,0 +1,148 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab
+import os
+import rushhour
+
+
+
+ngpu = 1
+latent_size = 64
+hidden_size = 256
+image_size = 36
+num_epochs = 300
+batch_size = 32
+
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+data_loader = rushhour.dataset(bs = batch_size)
+
+
+
+
+
+
+# Discriminator
+D = nn.Sequential(
+    nn.Linear(image_size, hidden_size),
+    nn.LeakyReLU(0.2),
+    nn.Linear(hidden_size, hidden_size),
+    nn.LeakyReLU(0.2),
+    nn.Linear(hidden_size, 1),
+    nn.Sigmoid())
+
+# Generator 
+G = nn.Sequential(
+    nn.Linear(latent_size, hidden_size),
+    nn.ReLU(),
+    nn.Linear(hidden_size, hidden_size),
+    nn.ReLU(),
+    nn.Linear(hidden_size, image_size),
+    nn.Tanh())
+
+G = G.to(device)
+D = D.to(device)
+
+
+criterion = nn.BCELoss()
+d_optimizer = torch.optim.Adam(D.parameters(), lr=0.0002)
+g_optimizer = torch.optim.Adam(G.parameters(), lr=0.0002)
+
+
+def reset_grad():
+    d_optimizer.zero_grad()
+    g_optimizer.zero_grad()
+
+
+d_losses = np.zeros(num_epochs)
+g_losses = np.zeros(num_epochs)
+real_scores = np.zeros(num_epochs)
+fake_scores = np.zeros(num_epochs)
+
+
+
+# Start training
+total_step = len(data_loader)
+for epoch in range(num_epochs):
+    for i, (images, _) in enumerate(data_loader):
+        images = images.view(batch_size, -1).cuda()
+        images = Variable(images)
+        # Create the labels which are later used as input for the BCE loss
+        real_labels = torch.ones(batch_size, 1).cuda()
+        real_labels = Variable(real_labels)
+        fake_labels = torch.zeros(batch_size, 1).cuda()
+        fake_labels = Variable(fake_labels)
+
+        # ================================================================== #
+        #                      Train the discriminator                       #
+        # ================================================================== #
+
+        # Compute BCE_Loss using real images where BCE_Loss(x, y): - y * log(D(x)) - (1-y) * log(1 - D(x))
+        # Second term of the loss is always zero since real_labels == 1
+        outputs = D(images)
+        d_loss_real = criterion(outputs, real_labels)
+        real_score = outputs
+        
+        # Compute BCELoss using fake images
+        # First term of the loss is always zero since fake_labels == 0
+        z = torch.randn(batch_size, latent_size).cuda()
+        z = Variable(z)
+        fake_images = G(z)
+        outputs = D(fake_images)
+        d_loss_fake = criterion(outputs, fake_labels)
+        fake_score = outputs
+        
+        # Backprop and optimize
+        # If D is trained so well, then don't update
+        d_loss = d_loss_real + d_loss_fake
+        reset_grad()
+        d_loss.backward()
+        d_optimizer.step()
+        # ================================================================== #
+        #                        Train the generator                         #
+        # ================================================================== #
+
+        # Compute loss with fake images
+        z = torch.randn(batch_size, latent_size).cuda()
+        z = Variable(z)
+        fake_images = G(z)
+        outputs = D(fake_images)
+        
+        # We train G to maximize log(D(G(z)) instead of minimizing log(1-D(G(z)))
+        # For the reason, see the last paragraph of section 3. https://arxiv.org/pdf/1406.2661.pdf
+        g_loss = criterion(outputs, real_labels)
+        
+        # Backprop and optimize
+        # if G is trained so well, then don't update
+        reset_grad()
+        g_loss.backward()
+        g_optimizer.step()
+        # =================================================================== #
+        #                          Update Statistics                          #
+        # =================================================================== #
+        d_losses[epoch] = d_losses[epoch]*(i/(i+1.)) + d_loss.data[0]*(1./(i+1.))
+        g_losses[epoch] = g_losses[epoch]*(i/(i+1.)) + g_loss.data[0]*(1./(i+1.))
+        real_scores[epoch] = real_scores[epoch]*(i/(i+1.)) + real_score.mean().data[0]*(1./(i+1.))
+        fake_scores[epoch] = fake_scores[epoch]*(i/(i+1.)) + fake_score.mean().data[0]*(1./(i+1.))
+
+    plt.figure()
+    pylab.xlim(0, num_epochs + 1)
+    plt.plot(range(1, num_epochs + 1), d_losses, label='d loss')
+    plt.plot(range(1, num_epochs + 1), g_losses, label='g loss')    
+    plt.legend()
+    plt.savefig('loss.pdf')
+    plt.close()
+
+    plt.figure()
+    pylab.xlim(0, num_epochs + 1)
+    pylab.ylim(0, 1)
+    plt.plot(range(1, num_epochs + 1), fake_scores, label='fake score')
+    plt.plot(range(1, num_epochs + 1), real_scores, label='real score')    
+    plt.legend()
+    plt.savefig('accuracy.pdf')
+    plt.close()
+    torch.save(G.state_dict(), 'G.ckpt')
+    torch.save(D.state_dict(), 'D.ckpt')
---
+++ rushhour.py
@@ -0,0 +1,85 @@
+import torch
+import numpy as np
+import string
+from PIL import Image, ImageDraw
+import os
+
+datadir = "rush.txt"
+
+def transform(rush):
+    '''
+    Takes in a rush hour string as defined in https://www.michaelfogleman.com/rush/ and transforms it into
+    a 6x6 numpy array where 0 is empty, 1 is the main vehicle, -1 is a wall, and all other numbers represent the other vehicles
+    '''
+
+    board = np.zeros((6,6))
+
+    for count, value in enumerate(rush):
+        i,j = count//6 ,count%6
+
+        if value == 'o':
+            board[i][j] = 0
+        elif value == 'A':
+            board[i][j] = 1
+        elif value == 'x':
+            board[i][j] = -1
+        else:
+            board[i][j] = string.ascii_lowercase.index(value)
+
+    return board 
+
+
+def dataset(bs):
+    '''
+    will return the first (or random, depending on order) elements of the rush hour dataset as flattened np arrays.
+    '''
+    if os.path.exists('rushnumpy.txt'):
+        data = np.loadtxt('rushnumpy.txt')
+    else:
+        data = np.genfromtxt(datadir, dtype= str)[:,1]
+        rush = np.array([transform(x).flatten() for x in data])
+        np.savetxt("rushnumpy.txt", rush)
+
+    data_loader = torch.utils.data.DataLoader(dataset=rush,
+                                          batch_size=bs, 
+                                          shuffle=True)
+
+    return(data_loader)
+def draw(rush):
+    '''
+    TODO: Finish this someday
+    Takes in a rush hour board and shows an image of it
+    ''' 
+    height = 600
+    width = 600
+
+    boardColor = "F2EACD"
+    blockedColor = "D96D60"
+    gridLineColor     = "222222"
+    primaryPieceColor = "CC3333"
+    pieceColor        = "338899"
+    pieceOutlineColor = "222222"
+    labelColor        = "222222"
+    wallColor         = "222222"
+
+    step_count = 6
+
+
+    image = Image.new("RGB", size=(height, width), color = boardColor) 
+    draw = ImageDraw.Draw(image)
+
+    y_start = 0
+    y_end = image.height
+    step_size = int(image.width / step_count)
+    for x in range(0, image.width, step_size):
+        line = ((x, y_start), (x, y_end))
+        draw.line(line, fill=128)
+    x_start = 0
+    x_end = image.width
+
+    for y in range(0, image.height, step_size):
+        line = ((x_start, y), (x_end, y))
+        draw.line(line, fill=128)
+    del draw
+
+
