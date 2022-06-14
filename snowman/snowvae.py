b02dcd1912dbebc0143064d71748e2c33946fa6d
Karesto <ahmedhachoukarah@gmail.com>
2022-06-14 10:43:48 +0200

    added layers

--- snowman/snowvae.py
+++ snowman/snowvae.py
@@ -22,39 +22,43 @@ class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
 
+
 class UnFlatten(nn.Module):
-    def forward(self, input, size=1024):
-        return input.view(input.size(0), size, 1, 1)
+    def forward(self, input, size = 25):
+        return input.view(input.size(0), 1, size, size)
+
 
 class VAE(nn.Module):
-    def __init__(self, image_channels=9, h_dim=size, z_dim=4):
+    def __init__(self, image_channels=9, h_dim=25, z_dim=32):
         super(VAE, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(image_channels, 8, kernel_size=3),
+            nn.Conv2d(image_channels, 8, kernel_size=3, padding=1),
             nn.BatchNorm2d(8),
             nn.ReLU(),
-            nn.Conv2d(8, 4, kernel_size=3),
+            nn.Conv2d(8, 4, kernel_size=3, padding=1),
             nn.BatchNorm2d(4),
             nn.ReLU(),
-            nn.Conv2d(4, 2, kernel_size=3),
+            nn.Conv2d(4, 2, kernel_size=3, padding=1),
             nn.BatchNorm2d(2),
             nn.ReLU(),
-            nn.Conv2d(2, 1, kernel_size=3),
-            nn.ReLU()
+            nn.Conv2d(2, 1, kernel_size=3, padding=1),
+            nn.ReLU(),
+            Flatten()
         )
         
-        self.mu = nn.Conv2d(1, z_dim, 3)
-        self.logvar = nn.Conv2d(1, z_dim, 3)
-        self.deassamble = nn.ConvTranspose2d(z_dim, h_dim, 3)
+        self.mu = nn.Linear(h_dim*h_dim, z_dim)
+        self.logvar = nn.Linear(h_dim*h_dim, z_dim)
+        self.back = nn.Linear(z_dim, h_dim*h_dim)
         
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(h_dim, 64, kernel_size=3),
+            UnFlatten(),
+            nn.ConvTranspose2d(1, 2, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, kernel_size=3),
+            nn.ConvTranspose2d(2, 4, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 16, kernel_size=3),
+            nn.ConvTranspose2d(4, 8, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(16, image_channels, kernel_size=3),
+            nn.ConvTranspose2d(8, image_channels, kernel_size=3, padding=1),
             nn.Softmax(),
         )
         
@@ -74,7 +78,7 @@ class VAE(nn.Module):
         return z, mu, logvar
 
     def decode(self, z):
-        z = self.deassamble(z)
+        z = self.back(z)
         z = self.decoder(z)
         return z
 
@@ -86,9 +90,10 @@ class VAE(nn.Module):
 
 
 cvae = VAE().to(device)
-optimizer = optim.Adam(cvae.parameters())
+optimizer = optim.Adam(cvae.parameters(), lr=0.00005)
 
 def loss_function(recon_x, x, mu, log_var):
+    #print(recon_x,x)
     BCE = F.binary_cross_entropy(recon_x, x.float(), reduction='sum')
     KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
     return BCE + KLD
@@ -98,7 +103,7 @@ def train(epoch):
     cvae.train()
     train_loss = 0
     for batch_idx, data in enumerate(snowdataset):
-        data= data.cuda()
+        data= data.to(device)
         optimizer.zero_grad()
         
         recon_batch, mu, log_var = cvae(data.float())
@@ -108,11 +113,10 @@ def train(epoch):
         train_loss += loss.item()
         optimizer.step()
         
+    print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss / len(snowdataset.dataset)))        
 
-        print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss / len(snowdataset.dataset)))        
 
-
-for epoch in range(1,101):
+for epoch in range(1,1001):
     train(epoch)
     
-torch.save(cvae, 'vae.pth')
+torch.save(cvae, 'vae-recep.pth')
